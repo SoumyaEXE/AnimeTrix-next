469c7eeb7e0e67a064e07eb68b74642e20f46338
Soumyadeep Dey <95275364+SoumyaEXE@users.noreply.github.com>
2023-12-16 23:29:39 +0530

    Update Footer.tsx

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -6,11 +6,11 @@ import { Github, Instagram } from "lucide-react";
 const socialMediaLinks = [
     {
         icon: <Github />,
-        url: "https://github.com/ShivaBhattacharjee/AnimeTrix-next",
+        url: "https://github.com/SoumyaEXE/AnimeTrix-next",
     },
     {
         icon: <Instagram />,
-        url: "https://www.instagram.com/animetrix.200/",
+        url: "https://www.instagram.com/s0umy4_xD/",
     },
 ];
 
@@ -19,7 +19,7 @@ const Footer = () => {
         <div className={`mb-20 md:mb-0 p-4 mt-12 md:mt-0 border-t border-white/20`}>
             <div className="flex flex-col gap-7 justify-between items-center">
                 <h1 className="text-3xl font-bold">AnimeTrix</h1>
-                <p className="text-[9px] md:text-sm text-center font-semibold m-auto max-w-4xl">AnimeTrix is not affiliated with or endorsed by any of the anime studios behind the creation of the anime presented on this site. This website is only a user interface presenting/linking various self-hosted files across the internet by other third-party providers for easy access. AnimeTrix never downloads the video from any source provider, link will be returned from the response hence it is completely not subjected to DMCA compliant.</p>
+                <p className="text-[9px] md:text-sm text-center font-semibold m-auto max-w-4xl">AnimeTrix is not affiliated with or endorsed by any of the anime studios behind the creation of the anime presented on this site. This website is only a user interface presenting/linking various self-hosted files across the internet by other third-party providers for easy access. AnimeTrix never downloads the video from any source provider, link will be returned from the response hence it is completely not subjected to DMCA compliant. ~ Soumyadeep Dey</p>
             </div>
             <div className="w-full flex justify-center items-center m-auto">
                 <div className="flex justify-between mt-12 items-center">
